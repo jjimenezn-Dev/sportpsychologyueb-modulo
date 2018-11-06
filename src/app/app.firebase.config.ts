44f3b3dbb2e7dc6529e0dcd8856a563d73274496
Sebastian Jimenez Nieto <juan_jimenezn@hotmail.com>
2018-11-05 23:49:45 -0500

    firebase integration

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,9 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyCMlMSwVyuMjgIbxsNf5zidkZ2Q2Thnstg",
+    authDomain: "sport-psychology.firebaseapp.com",
+    databaseURL: "https://sport-psychology.firebaseio.com",
+    projectId: "sport-psychology",
+    storageBucket: "sport-psychology.appspot.com",
+    messagingSenderId: "391756264916"
+  };
+;
